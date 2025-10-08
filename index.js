eb42016cb70b805d2c974f5b8015f9c83660e7a6
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2025-10-08 21:55:28 +0600

    remove extra line

--- index.js
+++ index.js
@@ -6,13 +6,8 @@ app.use(express.json());
 
 app.use(cors())
 
-
-
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-//require('dotenv').config();
-//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1jlx3rd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 const uri = "mongodb+srv://mezbahul:2A3NW9ZuLLtGXaGu@cluster0.1jlx3rd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 
@@ -66,7 +61,7 @@ async function run() {
       res.send(result);
 
     })
-
+  //change  data from the database
     app.put('/plants/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) }
@@ -81,84 +76,7 @@ async function run() {
       res.send(result);
 
     })
-
     
-
-    
-
-    // **********************************************************************************
-
-
-    //connection or create a new db as name coffees
-    const coffeesCollection = client.db('coffeeDB').collection('coffees');
-
-
-    // create localhost:3000/coffees 
-    app.get('/coffees', async (req, res) => {
-      // const cursor = coffeesCollection.find();
-      // const result = await cursor.toArray();
-      const result = await coffeesCollection.find().toArray();
-      res.send(result);
-    });
-
-    //create dynamic coffee details data
-    app.get('/coffees/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await coffeesCollection.findOne(query);
-      res.send(result);
-    });
-
-    //update coffee 
-    app.put('/coffees/:id', async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) }
-      const options = { upsert: true };
-      const updatedCoffee = req.body;
-      const updatedDoc = {
-        $set: updatedCoffee
-
-      }
-      // const updatedDoc = {
-      //     $set: {
-      //         name: updatedCoffee.name, 
-      //         supplier: updatedCoffee.supplier
-      //     }
-      // }
-      const result = await coffeesCollection.updateOne(filter, updatedDoc, options);
-
-      res.send(result);
-
-    })
-
-    // ......................................................................
-    const usersCollection = client.db('coffeeDB').collection('users');
-
-    // User related APIs
-
-  
-
-    app.delete('/users/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await usersCollection.deleteOne(query);
-      res.send(result);
-    })
-
-    //change passific data like lastSignInTime
-    app.patch('/users', async (req, res) => {
-      const { email, lastSignInTime } = req.body;
-      const filter = { email: email }
-      const updatedDoc = {
-        $set: {
-          lastSignInTime: lastSignInTime
-        }
-      }
-
-      const result = await usersCollection.updateOne(filter, updatedDoc)
-      res.send(result);
-    })
-
     // Send a ping to confirm a successful connection
     //await client.db("admin").command({ ping: 1 });
 
@@ -173,12 +91,10 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Plant Care Database')
 })
 
-app.get('/data', (req, res) => {
-  res.send('this is data')
-})
+
 
 
 app.listen(port, () => {
